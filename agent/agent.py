c3f87f7bdb0ed6c175e535a2e6d717766b8f4690
nmonkeyme <nmonkeyme@kali.kali>
2026-07-08 23:52:19 +0530

    cleanup before app integration

--- agent/agent.py
+++ agent/agent.py
@@ -1,10 +1,95 @@
-class FirstContactAgent:
+from armoriq_sdk import ArmorIQClient
 
-    def capture_plan(self):
-        print("capture_plan()")
 
-    def get_intent_token(self):
-        print("get_intent_token()")
+class RuleBasedAgent:
 
-    def invoke(self):
-        print("invoke()")
+    def __init__(self):
+        self.client = ArmorIQClient()
+
+
+    # -------------------------
+    # Rule based planner
+    # -------------------------
+
+    def create_plan(self, prompt):
+
+        if prompt == "show_sales":
+
+            return {
+                "steps": [
+                    {
+                        "mcp": "sales-mcp",
+                        "action": "fetch_sales",
+                        "params": {}
+                    }
+                ]
+            }
+
+
+        elif prompt == "analyze_sales":
+
+            return {
+                "steps": [
+                    {
+                        "mcp": "sales-mcp",
+                        "action": "analyze_sales",
+                        "params": {}
+                    }
+                ]
+            }
+
+
+        else:
+            raise Exception("Unknown command")
+
+
+    # -------------------------
+    # Execute through ArmorIQ
+    # -------------------------
+
+    def execute(self, prompt):
+
+        plan = self.create_plan(prompt)
+
+
+        print("\nPLAN:")
+        print(plan)
+
+
+        # Capture execution plan
+        plan_capture = self.client.capture_plan(
+            llm="rule-based-planner",
+            prompt=prompt,
+            plan=plan
+        )
+
+
+        print("\nPLAN CAPTURE:")
+        print(plan_capture)
+
+
+
+        # Generate intent token
+        intent_token = self.client.get_intent_token(
+            plan_capture
+        )
+
+
+        print("\nINTENT TOKEN GENERATED")
+
+
+
+        # Get first step
+        step = plan["steps"][0]
+
+
+        # Execute through ArmorIQ MCP proxy
+        result = self.client.invoke(
+            mcp=step["mcp"],
+            action=step["action"],
+            intent_token=intent_token,
+            params=step["params"]
+        )
+
+
+        return result
---
+++ test_sdk.py
@@ -0,0 +1,5 @@
+from armoriq_sdk import ArmorIQClient
+
+client = ArmorIQClient.from_config("mcp/armoriq.yaml")
+
+print(client)
